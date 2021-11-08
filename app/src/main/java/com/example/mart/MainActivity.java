a94a1aa881017ccba9ab7ca16f54148a899de8cc
Amey22s <ameygurunathparab@gmail.com>
2021-11-09 00:07:06 +0530

    Draft1-Basic skeleton

---
+++ app/src/main/java/com/example/mart/MainActivity.java
@@ -0,0 +1,179 @@
+package com.example.mart;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.example.mart.ui.main.SectionsPagerAdapter;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    NavigationView nav;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    FloatingActionButton fab,fab1,fab2;
+    Animation fabOpen,fabClose,rotateFwd,rotateBack;
+    boolean isOpen = false;
+
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        if (toolbar != null)
+        {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setElevation(0); // or other
+        }
+
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.menu);
+        nav = (NavigationView)findViewById(R.id.navmenu);
+
+
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.toggle_open,R.string.toggle_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+
+        fabOpen = AnimationUtils.loadAnimation(this,R.anim.fab_open);
+        fabClose = AnimationUtils.loadAnimation(this,R.anim.fab_close);
+        rotateFwd = AnimationUtils.loadAnimation(this,R.anim.rotate_forward);
+        rotateBack = AnimationUtils.loadAnimation(this,R.anim.rotate_backward);
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frag_con,new Home()).commit();
+
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            Fragment frag;
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId())
+                {
+                    case R.id.home:
+                        frag = new Home();
+                        break;
+
+                    case R.id.orders:
+                        frag = new Orders();
+                        break;
+
+                    case R.id.categories:
+                        frag = new Categories();
+                        break;
+
+                    case R.id.contact:
+                        frag = new Contact();
+                        break;
+
+                    case R.id.logout:
+                        frag = new Logout();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frag_con,frag).commit();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animatefab();
+            }
+
+        });
+
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animatefab();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frag_con,new Cart()).commit();
+
+            }
+
+        });
+
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animatefab();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frag_con,new Settings()).commit();
+            }
+
+        });
+
+
+    }
+
+    private void animatefab() {
+        if(isOpen)
+        {
+            fab.startAnimation(rotateFwd);
+            fab1.startAnimation(fabClose);
+            fab2.startAnimation(fabClose);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            isOpen=false;
+        }
+        else
+        {
+            fab.startAnimation(rotateBack);
+            fab1.startAnimation(fabOpen);
+            fab2.startAnimation(fabOpen);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            isOpen=true;
+        }
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawers();
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        // Here I tell Android to inflate your menu
+        inflater.inflate(R.menu.drawer_menu, menu);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/mart/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,57 @@
+package com.example.mart.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.mart.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    private List <Fragment> fragmentList = new ArrayList<>();
+    private List <String> titleList = new ArrayList<>();
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return fragmentList.get(position);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titleList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title)
+    {
+        fragmentList.add(fragment);
+        titleList.add(title);
+    }
+}
